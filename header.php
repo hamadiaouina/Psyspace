fbb41c6a06d9c861a071e37e1c03df9fbd2be326
hamadiaouina <hamadi.aouina@etudiant-isi.utm.tn>
2026-04-28 18:58:20 +0100

    trying de fix logo

--- header.php
+++ header.php
@@ -124,7 +124,7 @@ ob_start(function($buffer) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" type="image/png" href="{{ asset('assets/images/logo.png') }}">    <title>PsySpace | Espace Thérapeutique</title>
-
+<link rel="shortcut icon" href="/assets/logo.png">
     <script src="assets/js/tailwind.min.js" nonce="<?= $nonce ?>"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
